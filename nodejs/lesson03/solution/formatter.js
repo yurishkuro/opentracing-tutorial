bf4768e4672302469555f2e09de751ab196b6b70
junminstorage <junmin.storage@gmail.com>
2017-10-02 18:47:18 -0400

    UPDATE: introduce nodejs request promise chaining example

---
+++ nodejs/lesson03/solution/formatter.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express')
+const app = express()
+const initTracer = require('../../lib/tracing').initTracer;
+const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
+
+const tracer = initTracer('format-service');
+
+const port = 8081;
+
+app.listen(port, function () {
+    console.log('Formatter app listening on port ' + port);
+})
+
+app.get('/format/:str', function (req, res) {
+    const parentSpanContext = tracer.extract(FORMAT_HTTP_HEADERS, req.headers)
+    const span = tracer.startSpan('http_server', {
+        childOf: parentSpanContext
+    });
+    span.setTag(Tags.SPAN_KIND, Tags.SPAN_KIND_RPC_SERVER);
+
+    const str= req.params.str;
+
+    span.log({
+        'event': 'format',
+        'value': str
+    });
+
+    span.finish();
+
+    res.send('Hello, ' +  str);
+})
+
+
+
