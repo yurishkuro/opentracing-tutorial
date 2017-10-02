b671f01014924f36ab2ce0de1d381cf0b3f92606
junminstorage <junmin.storage@gmail.com>
2017-10-02 18:46:26 -0400

    UPDATE: introduce nodejs request promise chaining example

--- nodejs/lesson03/solution/hello.js
+++ nodejs/lesson03/solution/hello.js
@@ -1,109 +1,94 @@
 'use strict';
 
-var express = require('express')
-var app = express()
-var initTracer = require('../../lib/tracing').initTracer;
+const express = require('express')
+const app = express()
+const initTracer = require('../../lib/tracing').initTracer;
+var request = require('request-promise');
 
 const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
 
-var request = require('request');
+const tracer = initTracer('hello-world');
 
-app.get('/', function (req, res) {
-    let url = 'localhost:8081', method = 'GET';
+const port = 8080;
 
-    var helloStr = request(url, method, helloTo);
-    res.send(helloStr)
+app.listen(port, function () {
+    console.log('Hello app listening on port ' + port);
 })
 
-app.get('/lesson03', function (req, res){
+// main endpoint
+app.get('/:str', main);
 
-// Request options
-const uri = 'http://localhost:8081'
-const method = 'GET'
-const headers = {}
-// Start a span
-const span = tracer.startSpan('http_request')
-span.setTag(Tags.HTTP_URL, uri)
-span.setTag(Tags.HTTP_METHOD, method)
+function main(req, res) {
+    const input = req.params.str;
 
-// Send span context via request headers (parent id etc.)
-tracer.inject(span, FORMAT_HTTP_HEADERS, headers)
-
-request({ uri, method, headers }, (err, data) => {
-    console.log(err, data);
-
-  // Error handling
-  if (err) {
-    span.setTag(Tags.ERROR, true)
-    span.setTag(Tags.HTTP_STATUS_CODE, err.statusCode)
-    span.log({
-      event: 'error',
-      message: err.message,
-      err
-    })
-    span.finish()
-    res.send('error')
-    return
-  }
-
-  // Finish span
-  span.setTag(Tags.HTTP_STATUS_CODE, data.statusCode)
-  span.finish()
-  res.send('ok')
-})
-
-
-})
-
-function sayHello(helloTo) {
-    var span = tracer.startSpan('say-hello');
-    
-    var helloStr = format_string(helloTo, span);
+    const span = tracer.startSpan('say-hello-request');
 
     span.log({
-        'event': 'sayHello',
-        'value': helloStr
+        'event': 'sayHelloRequest',
+        'value': input
     });
-    
-    print_string(helloStr, span);
-    span.log({'event': 'print-string'})
-    
-    span.finish();
 
-    return helloStr;
-}  
+    format_string(input, span)
+        .then( data => {
+            return print_hello(data, span);
+        })
+        .then( data => {
+            span.setTag(Tags.HTTP_STATUS_CODE, 200)
+            span.finish()
+            res.send('Got response back from service: ' + data);
+        })
+        .catch( err => {
+            console.error(err.message);
+            span.setTag(Tags.ERROR, true)
+            span.setTag(Tags.HTTP_STATUS_CODE, err.statusCode || 500);
+            span.finish();
+            res.send('Sorry, an error happened during call downstream service!');
+        });
 
-function format_string(helloTo, root_span) {
-    var span = tracer.startSpan('format_string', {childOf: root_span.context()});
-    var formattedStr = 'Hello, ' + helloTo + '!';
+}
 
+function format_string(input, root_span) {
+    const url = 'http://localhost:8081/format/' + input;
+    const fn = 'format';
+
+    const span = tracer.startSpan(fn, {childOf: root_span.context()});
     span.log({
         'event': 'format-string',
-        'value': helloTo
+        'value': input
     });
 
-    span.finish();    
-    return formattedStr;
-}
+    return http_get(fn, url, span); 
+}  
 
-function print_string(helloStr, root_span) {
-    var span = tracer.startSpan('print_string', {childOf: root_span.context()});
+function print_hello(input, root_span) {
+    const url = 'http://localhost:8082/publish/' + input;
+    const fn = 'publish';
+
+    const span = tracer.startSpan(fn, {childOf: root_span.context()});
     span.log({
         'event': 'print-string',
-        'value': helloStr
+        'value': input
     });
-    console.log(helloStr);
-    span.finish();
+    return http_get(fn, url, span);
 }
+
+function http_get(fn, url, span) {
+    const method = 'GET';
+    const headers = {};
+    
+    span.setTag(Tags.HTTP_URL, url);
+    span.setTag(Tags.HTTP_METHOD, method);
+    span.setTag(Tags.SPAN_KIND, Tags.SPAN_KIND_RPC_CLIENT);
+    // Send span context via request headers (parent id etc.)
+    tracer.inject(span, FORMAT_HTTP_HEADERS, headers);
+
+    return request({url, method, headers})
+            .then( data => {
+                span.finish();
+                return data;
+            });
+
+}
+
   
-if (process.argv.length != 3) {
-    throw new Error('expecting one argument')
-}
 
-var helloTo = process.argv[2];
-
-var tracer = initTracer('hello-world');
-
-app.listen(8080, function () {
-    console.log('Hello app listening on port 8080')
-})
