f2c5f4b97a6502c8fa18dc3d7c36925c92448885
junminstorage <junmin.storage@gmail.com>
2017-10-08 01:00:20 -0400

    UPDATE: set timeout for UDP message being sent out

--- nodejs/lesson01/solution/hello.js
+++ nodejs/lesson01/solution/hello.js
@@ -27,6 +27,18 @@ const tracer = initTracer('hello-world');
 
 sayHello(helloTo);
 
-//flush out the span and close any reporters and senders
-tracer.close();
+//wait for udp message sent out
+setTimeout( e => {tracer.close();}, 12000);
+
+/* another way to make sure udp message is sent out is by listening to the message event
+const server = require('dgram').createSocket('udp4');
+
+server.on('message', (msg, rinfo) => {
+  tracer.close();
+  server.close();
+});
+
+server.bind(6832);
+*/
+
 
