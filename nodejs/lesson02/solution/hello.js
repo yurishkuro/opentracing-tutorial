e3ff73065172b06999204e8b2ff194bfafc4feb4
junminstorage <junmin.storage@gmail.com>
2017-10-07 23:16:17 -0400

    UPDATE: rewrite it as js script

--- nodejs/lesson02/solution/hello.js
+++ nodejs/lesson02/solution/hello.js
@@ -1,26 +1,15 @@
 'use strict';
 
-var express = require('express')
-var app = express()
+var assert = require('assert');
 var initTracer = require('../../lib/tracing').initTracer;
-
-app.get('/', function (req, res) {
-    var helloStr = sayHello(helloTo);
-    res.send(helloStr)
-})
 
 function sayHello(helloTo) {
     var span = tracer.startSpan('say-hello');
-    
-    var helloStr = format_string(helloTo, span);
+    span.setTag('hello-to', helloTo);
 
-    span.log({
-        'event': 'sayHello',
-        'value': helloStr
-    });
+    var helloStr = format_string(helloTo, span);
     
     print_string(helloStr, span);
-    span.log({'event': 'print-string'})
     
     span.finish();
 
@@ -29,7 +18,7 @@ function sayHello(helloTo) {
 
 function format_string(helloTo, root_span) {
     var span = tracer.startSpan('format_string', {childOf: root_span.context()});
-    var formattedStr = 'Hello, ' + helloTo + '!';
+    var formattedStr = `Hello, ${helloTo}!`;
 
     span.log({
         'event': 'format-string',
@@ -42,22 +31,22 @@ function format_string(helloTo, root_span) {
 
 function print_string(helloStr, root_span) {
     var span = tracer.startSpan('print_string', {childOf: root_span.context()});
+
+    console.log(helloStr);
+
     span.log({
         'event': 'print-string',
         'value': helloStr
     });
-    console.log(helloStr);
     span.finish();
 }
   
-if (process.argv.length != 3) {
-    throw new Error('expecting one argument')
-}
+assert.ok(process.argv.length == 3, 'expecting one argument');
 
 var helloTo = process.argv[2];
 
 var tracer = initTracer('hello-world');
 
-app.listen(8080, function () {
-    console.log('Hello app listening on port 8080')
-})
+sayHello(helloTo);
+
+tracer.close();
