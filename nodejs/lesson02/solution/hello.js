1a0fb442c309279674a2cbc4fe582686f394e7b9
Kara de la Marck <karadelamarck@gmail.com>
2018-02-07 10:16:27 +0000

    Add lesson02 solution
    Follows up to current lesson02 README.md

--- nodejs/lesson02/solution/hello.js
+++ nodejs/lesson02/solution/hello.js
@@ -1,52 +1,39 @@
-'use strict';
+const assert = require("assert");
+const { initTracer } = require("../../lib/tracing");
 
-const assert = require('assert');
-const initTracer = require('../../lib/tracing').initTracer;
+const tracer = initTracer("hello-world");
 
-function sayHello(helloTo) {
-    var span = tracer.startSpan('say-hello');
-    span.setTag('hello-to', helloTo);
+const sayHello = helloTo => {
+  const span = tracer.startSpan("say-hello");
+  span.setTag("hello-to", helloTo);
+  const helloStr = formatString(span, helloTo);
+  printHello(span, helloStr);
+  span.finish();
+};
 
-    var helloStr = format_string(helloTo, span);
-    
-    print_string(helloStr, span);
-    
-    span.finish();
+const formatString = (rootSpan, helloTo) => {
+  const span = tracer.startSpan("format", { childOf: rootSpan.context() });
+  const helloStr = `Hello, ${helloTo}!`;
+  span.log({
+    event: "string-format",
+    value: helloStr,
+  });
+  span.finish();
+  return helloStr;
+};
 
-    return helloStr;
-}  
+const printHello = (rootSpan, helloStr) => {
+  const span = tracer.startSpan("consoleLog", { childOf: rootSpan.context() });
+  console.log(helloStr);
+  span.log({ event: "print-string" });
+  span.finish();
+};
 
-function format_string(helloTo, root_span) {
-    var span = tracer.startSpan('format_string', {childOf: root_span.context()});
-    var formattedStr = `Hello, ${helloTo}!`;
-
-    span.log({
-        'event': 'format-string',
-        'value': helloTo
-    });
-
-    span.finish();    
-    return formattedStr;
-}
-
-function print_string(helloStr, root_span) {
-    var span = tracer.startSpan('print_string', {childOf: root_span.context()});
-
-    console.log(helloStr);
-
-    span.log({
-        'event': 'print-string',
-        'value': helloStr
-    });
-    span.finish();
-}
-  
-assert.ok(process.argv.length == 3, 'expecting one argument');
-
+assert.ok(process.argv.length == 3, "Expecting one argument");
 const helloTo = process.argv[2];
-
-const tracer = initTracer('hello-world');
 
 sayHello(helloTo);
 
-setTimeout( e => {tracer.close();}, 12000);
+const closeTracer = () => tracer.close();
+
+setTimeout(closeTracer, 12000);
