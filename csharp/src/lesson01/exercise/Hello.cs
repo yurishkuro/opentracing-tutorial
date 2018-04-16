580bca1cb95f840bfe690586635308d6677d6d49
Sascha M Holesch <sascha.holesch@jungheinrich.de>
2018-04-16 15:33:42 +0200

    [MOD] Lesson 1: Initialize a tracker

--- csharp/src/lesson01/exercise/Hello.cs
+++ csharp/src/lesson01/exercise/Hello.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.SqlTypes;
+using OpenTracing.Util;
 
-namespace OpenTracing.Tutorial.Lesson01.Solution
+namespace OpenTracing.Tutorial.Lesson01.Exercise
 {
     internal class Hello
     {
-        
+        private readonly ITracer tracer;
+
+        public Hello(OpenTracing.ITracer tracer)
+        {
+            this.tracer = tracer;
+        }
+
+        public void SayHello(string helloTo)
+        {
+            var span = tracer.BuildSpan("say-hello").Start();
+            var helloString = $"Hello, {helloTo}!";
+            Console.WriteLine(helloString);
+            span.Finish();
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("Expecting one argument");
+            }
+
+            var helloTo = args[0];
+            new Hello(GlobalTracer.Instance).SayHello(helloTo);
+        }
     }
 }
