27fbae9681679c9fc4b009e8263c0bd44e9a7802
Sascha Holesch <Sascha.Holesch@jungheinrich.de>
2018-04-18 12:56:12 +0200

    [MOD] Exercise base finished

---
+++ csharp/src/lesson03/exercise/Lesson03.Exercise/Controllers/PublishController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenTracing.Util;
+
+namespace Lesson03.Exercise.Controllers
+{
+    [Route("api/publish")]
+    public class PublishController : Controller
+    {
+        // GET: api/publish
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello!";
+        }
+
+        // GET: api/publish/helloString
+        [HttpGet("{helloString}", Name = "GetPublish")]
+        public string Get(string helloString)
+        {
+            Console.WriteLine(helloString);
+            return "published";
+        }
+    }
+}
--- csharp/src/lesson03/exercise/Lesson03.Exercise/Startup.cs
+++ csharp/src/lesson03/exercise/Lesson03.Exercise/Startup.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Jaeger.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using OpenTracing.Util;
+using OpenTracing.Tutorial.Library;
 
 namespace Lesson03.Exercise
 {
     public class Startup
     {
+        private static readonly Tracer Tracer = Tracing.Init("Webservice");
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,10 +30,14 @@ namespace Lesson03.Exercise
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(Configuration.GetSection("Logging"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime applicationLifetime)
         {
             if (env.IsDevelopment())
             {
