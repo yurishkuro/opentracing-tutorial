e93365db1d40442a1e559d8d513c6e181e37ead9
Sascha Holesch <sascha.holesch@jungheinrich.de>
2018-04-19 13:42:42 +0200

    [ADD] Empty Project for Exercise

---
+++ csharp/src/lesson03/exercise/Lesson03.Exercise.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson03.Exercise.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
